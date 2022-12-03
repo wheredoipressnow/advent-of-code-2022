70b0b2e7dc98b04b179d0a6b8b0013377749a904
LarsO <lars.oehnenamen@gmail.com>
2022-12-03 21:04:47 +0200

    wip - day 3 puzzle 1

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val t =TEST3_INPUT.split("\n")
+        .map{            it.chunked(it.length / 2)        }
+    println(t)
+}
+
+private const val TEST3_INPUT =
+    """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
